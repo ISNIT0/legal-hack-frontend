723c7e03c82e990496adde6d41ff0881462868fb
Joe Reeve <joseph.reeve@googlemail.com>
2020-03-07 14:19:49 +0000

    Fix build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
+const mode = process.env.NODE_ENV;
 export default {
     input: 'src/index.js',
     output: {
@@ -8,6 +10,10 @@ export default {
         format: 'iife'
     },
     plugins: [
+        replace({
+            'process.browser': true,
+            'process.env.NODE_ENV': JSON.stringify(mode)
+        }),
         resolve({
             browser: true,
         }),
